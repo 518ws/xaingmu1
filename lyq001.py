6db633e845c3534d049a3c53810bf69505b68005
518ws <2715530792@qq.com>
2022-04-02 09:27:53 +0800

    修改上传

---
+++ lyq001.py
@@ -0,0 +1,34 @@
+# a=input ("请输入：")
+# b=input ("请输入:")
+# print("input获取的内容:",a+b)
+
+# print(2<3)
+
+
+# a=float(input ("请输入："))
+# b=float(input ("请输入:"))
+# print("input获取的内容:",a+b)
+
+# print(type (2333))
+
+
+
+
+# a="sjasjsos手机上惊声尖叫   sjjsjsjs"
+# print (len (a ))
+
+# a=str(2333)
+# print(type (a))
+
+# a=int (3.9)
+# print (a)
+
+# a=len ("zzzzzzzzzzzz")
+# b=len ("zzzzzzzzzzzz")
+# print(a+b)
+
+a=input ("请输入：")
+b=input ("请输入：")
+print(len (a)+len (b))
+
+
