a596734d6acfeadd4d3c42aed6b3c293547b9c06
518ws <2715530792@qq.com>
2022-04-02 16:01:41 +0800

    乱七八糟

--- lyq001.py
+++ lyq001.py
@@ -4,15 +4,11 @@
 
 # print(2<3)
 
-
 # a=float(input ("请输入："))
 # b=float(input ("请输入:"))
 # print("input获取的内容:",a+b)
 
 # print(type (2333))
-
-
-
 
 # a="sjasjsos手机上惊声尖叫   sjjsjsjs"
 # print (len (a ))
@@ -31,9 +27,69 @@
 # b=input ("请输入：")
 # print(len (a)+len (b))
 
+# a=(2,3,8,5,"wangsheng","往生","wuyou")
+# print (a[6])
+
+# a=(1,2,"ws","wangsheng","往生","往生","2")
+# print (a.count("往生") )
+
+# print (a.index("往生") )
+
+# a=(1,2,"ws","wangsheng","往生","往生","2")
+# print (a [0:3])
+# print (a[3:5])
+# print (a[5:])
 
 
-a=input ("请输入：")
-b=input ("请输入：")
-print(len (a)+len (b))
+# b=(a,"wuyou","无忧")
+# print(b[0][3])
+
+# a=[1,2,"ws","wangsheng","往生","往生","2"]
+# print (a.count ("往生"))
+
+
+
+# a=[1,2,"ws","wangsheng","往生","往生","2"]
+# a.insert (2,"wuyou")
+# print(a)
+
+
+# a=[1,2,"ws","wangsheng","往生","往生","2"]
+# b=a.pop (3)
+# print (a)
+# print(b)
+
+# a=[1,2,"ws","wangsheng","往生","往生","2"]
+# b=["wuyou","无忧"]
+# print(a+b)
+
+
+
+
+# a=[1,2,"ws","wangsheng","往生","往生","2"]
+# a.clear()
+# print (a)
+
+# from html import entities
+
+
+# a=[1,2,"ws","wangsheng","往生","往生","2"]
+# a .extend (2022)
+# print(a) 
+
+# a=[1,2,"ws","wangsheng","往生","往生","2"]
+# a.remove("往生")
+# print (a)
+
+# a=[0,False]
+# print(a.count(0))
+
+a={"name":"ws","age":24}
+a["height"]=155
+print(a)
+
+
+
+
+
 
